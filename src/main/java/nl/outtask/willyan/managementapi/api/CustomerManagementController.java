0569dbd7e7bc51919f1b82e6f72f007ee3e9b017
Willyan Rodrigues <willyan.rodrigues@outtask.nl>
2024-01-04 11:31:18 +0100

    added get operation.

--- src/main/java/nl/outtask/willyan/managementapi/api/CustomerManagementController.java
+++ src/main/java/nl/outtask/willyan/managementapi/api/CustomerManagementController.java
@@ -3,10 +3,7 @@ package nl.outtask.willyan.managementapi.api;
 import lombok.extern.slf4j.Slf4j;
 import nl.outtask.willyan.managementapi.domain.dto.Customer;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -15,10 +12,21 @@ import java.net.URI;
 @Slf4j
 public class CustomerManagementController {
 
+    private Customer customer;
+
     @PostMapping
     private ResponseEntity<Void> createCustomer(@RequestBody final Customer customer) {
       log.info("Customer is {}", customer.toString());
+      this.customer = customer;
       return ResponseEntity.created(URI.create("/customer/1")).build();
+    }
+
+    @GetMapping("/1")
+    private ResponseEntity<Customer> findCustomer() {
+        if (null != customer) {
+            return ResponseEntity.ok(customer);
+        }
+        return ResponseEntity.notFound().build();
     }
 
 }
